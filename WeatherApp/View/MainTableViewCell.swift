b163f278a35c071765950299a823e726837e4ba3
daesung <eogud8203@naver.com>
2024-08-14 17:08:32 +0900

    [Feat] 테이블 뷰 및 셀 생성 테이블 뷰 헤더 구현
    [Fix]
    가독성을 위한 레이아웃 func 수정
    레이아웃 make in -> $0 통일
    
    [Bug]
    테이블 뷰 하단 cornerRadius적용 안됨에 따른 수정필요

---
+++ WeatherApp/View/MainTableViewCell.swift
@@ -0,0 +1,69 @@
+//
+//  MainCollectionViewCell.swift
+//  WeatherApp
+//
+//  Created by 머성이 on 8/14/24.
+//
+
+import UIKit
+import SnapKit
+
+class MainTableViewCell: UITableViewCell {
+
+    static let identifier = "MainTableViewCell"
+    
+    private let dayLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        return label
+    }()
+    
+    // 이미지 관련 (데이터 받아와야함)
+    private let weatherIcon: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private let temperatureLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        [dayLabel, weatherIcon, temperatureLabel].forEach {
+            contentView.addSubview($0)
+        }
+        
+        dayLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.left.equalToSuperview().offset(16)
+        }
+        
+        weatherIcon.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.left.equalTo(dayLabel.snp.right).offset(16)
+            $0.width.height.equalTo(24)
+        }
+        
+        temperatureLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.left.equalTo(weatherIcon.snp.right).offset(16)
+            $0.right.equalToSuperview().offset(-16)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // 정보 받아오는거 이쪽에서 수정 !!! 자료형도 변환해주셈
+    func configure(with day: String, weatherIconName: String, temperature: String) {
+        dayLabel.text = day
+        weatherIcon.image = UIImage(named: weatherIconName)
+        temperatureLabel.text = temperature
+    }
+}
--- WeatherApp/View/MainView.swift
+++ WeatherApp/View/MainView.swift
@@ -10,7 +10,7 @@ import SnapKit
 import RxSwift
 
 class MainView: UIView {
-
+    
     // MARK: - 레이블 폰트 설정 - DS
     private let systemFontLabel: (String, CGFloat, UIColor) -> UILabel = { text, fontSize, textColor in
         let label = UILabel()
@@ -31,12 +31,12 @@ class MainView: UIView {
     }
     
     // MARK: - 레이블 디테일 - DS
-    // 고정
+    // 고정값
     private lazy var locationLabel = boldSystemFontLabel("나의 위치", 32, .black)
     private lazy var humidityLabel = boldSystemFontLabel("습도", 16, .white)
     private lazy var sunriseLabel = boldSystemFontLabel("일출", 16, .white)
     private lazy var sunsetLabel = boldSystemFontLabel("일몰", 16, .white)
-
+    
     // 변동사항 있음
     private lazy var locationDetailLabel = systemFontLabel("서울시", 16, .black)
     private lazy var temperatureLabel = boldSystemFontLabel("30°C", 48, .black)
@@ -44,148 +44,216 @@ class MainView: UIView {
     private lazy var humidityDetailLabel = systemFontLabel("70%", 48, .white)
     private lazy var sunriseDetailLabel = boldSystemFontLabel("05:38", 16, .white)
     private lazy var sunsetDetailLabel = boldSystemFontLabel("19:56", 16, .white)
-
+    
     // MARK: - 섹션별 뷰
     private lazy var topView = UIView()
     private lazy var midView = UIView()
     private lazy var humidityContainer = UIView()
     private lazy var sunriseSunsetContainer = UIView()
     
+    // MARK: - 테이블 뷰
+    let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(MainTableViewCell.self, forCellReuseIdentifier: MainTableViewCell.identifier)
+        tableView.separatorStyle = .none
+        tableView.clipsToBounds = true
+        tableView.layer.cornerRadius = 10
+        tableView.isScrollEnabled = false
+        return tableView
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        configureUI()
         setupUI()
+        configureUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
+    private func setupUI() {
+        setupTopViewLayout()
+        setupMidViewLayout()
+        setupTableViewLayout()
+    }
+    
     private func configureUI() {
-        // MARK: - 상단 뷰 레이아웃
-        [locationLabel, locationDetailLabel, temperatureLabel, descriptionLabel].forEach {
-            topView.addSubview($0)
+        configureTopViewLayout()
+        configureMidViewLayout()
+        configureTableViewHeader()
+    }
+    
+    // MARK: - 상단, 중간 뷰 레이아웃
+    private func setupTopViewLayout() {
+        [topView, midView].forEach {
+            self.addSubview($0)
         }
         
-        locationLabel.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(88)
-            make.centerX.equalToSuperview()
-            make.height.greaterThanOrEqualTo(32)
+        // 상단 섹션 뷰
+        topView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.leading.equalToSuperview().offset(20)
+            $0.trailing.equalToSuperview().offset(-20)
+            $0.height.equalTo(self).multipliedBy(0.3)
         }
         
-        locationDetailLabel.snp.makeConstraints { make in
-            make.top.equalTo(locationLabel.snp.bottom).offset(8)
-            make.centerX.equalToSuperview()
-            make.height.greaterThanOrEqualTo(16)
+        // 중단 섹션 뷰
+        midView.snp.makeConstraints {
+            $0.top.equalTo(topView.snp.bottom).offset(16)
+            $0.leading.equalToSuperview().offset(20)
+            $0.trailing.equalToSuperview().offset(-20)
+            $0.height.equalTo(152)
         }
-        
-        temperatureLabel.snp.makeConstraints { make in
-            make.top.equalTo(locationDetailLabel.snp.bottom).offset(8)
-            make.centerX.equalToSuperview()
-            make.height.greaterThanOrEqualTo(48)
-        }
-        
-        descriptionLabel.snp.makeConstraints { make in
-            make.top.equalTo(temperatureLabel.snp.bottom).offset(8)
-            make.centerX.equalToSuperview()
-            make.height.greaterThanOrEqualTo(16)
-        }
-        
-       // MARK: - 중간 뷰 레이아웃
-
-        // 습도 관련
-        [humidityLabel, humidityDetailLabel].forEach {
-            humidityContainer.addSubview($0)
-        }
-
-        humidityLabel.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(8)
-            make.leading.equalToSuperview().offset(30)
-            make.height.equalTo(16)
-        }
-
-        humidityDetailLabel.snp.makeConstraints { make in
-            make.top.equalTo(humidityLabel.snp.bottom).offset(8)
-            make.leading.equalTo(humidityLabel.snp.leading)
-            make.bottom.equalToSuperview().offset(-16)
-            make.height.equalTo(48)
-        }
-
-        // 일출, 일몰 관련
-        [sunriseLabel, sunriseDetailLabel, sunsetLabel, sunsetDetailLabel].forEach {
-            sunriseSunsetContainer.addSubview($0)
-        }
-
-        sunriseLabel.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(24)
-            make.leading.equalToSuperview().offset(24)
-            make.height.equalTo(16)
-        }
-
-        sunriseDetailLabel.snp.makeConstraints { make in
-            make.top.equalTo(sunriseLabel.snp.bottom).offset(8)
-            make.leading.equalTo(sunriseLabel.snp.leading)
-            make.height.equalTo(16)
-        }
-
-        sunsetLabel.snp.makeConstraints { make in
-            make.top.equalTo(sunriseDetailLabel.snp.bottom).offset(24)
-            make.leading.equalTo(sunriseLabel.snp.leading)
-            make.height.equalTo(16)
-        }
-
-        sunsetDetailLabel.snp.makeConstraints { make in
-            make.top.equalTo(sunsetLabel.snp.bottom).offset(8)
-            make.leading.equalTo(sunriseLabel.snp.leading)
-            make.bottom.equalToSuperview().offset(-16)
-            make.height.equalTo(16)
-        }
-
-        // MARK: - 습도, 일출, 일몰 관련 뷰
+    }
+    
+    private func setupMidViewLayout() {
         [humidityContainer, sunriseSunsetContainer].forEach {
-            midView.addSubview($0) // 중간 뷰에 추가
+            midView.addSubview($0)
         }
-
-        humidityContainer.snp.makeConstraints { make in
-            make.leading.equalToSuperview()
-            make.centerY.equalToSuperview()
-            make.height.equalTo(152)
-            make.width.equalTo(152)
+        
+        // 습도 레이블 스택뷰
+        humidityContainer.snp.makeConstraints {
+            $0.leading.equalToSuperview()
+            $0.centerY.equalToSuperview()
+            $0.height.equalTo(136)
+            $0.width.equalTo(168)
         }
-
-        sunriseSunsetContainer.snp.makeConstraints { make in
-            make.trailing.equalToSuperview()
-            make.centerY.equalToSuperview()
-            make.height.equalTo(152)
-            make.width.equalTo(152)
+        
+        // 밀몰, 일출 레이블 스택뷰
+        sunriseSunsetContainer.snp.makeConstraints {
+            $0.trailing.equalToSuperview()
+            $0.centerY.equalToSuperview()
+            $0.height.equalTo(136)
+            $0.width.equalTo(168)
         }
-
-        // 컨테이너 배경색 및 둥근 모서리 설정
+        
+        // 스택뷰 디테일
         humidityContainer.backgroundColor = .systemBlue
         sunriseSunsetContainer.backgroundColor = .systemBlue
         humidityContainer.layer.cornerRadius = 10
         sunriseSunsetContainer.layer.cornerRadius = 10
     }
-
-    // MARK: - 상단, 중간 뷰 레이아웃
-    private func setupUI() {
-        self.addSubview(topView)
-        self.addSubview(midView)
-
-        topView.snp.makeConstraints { make in
-            make.top.equalToSuperview()
-            make.leading.equalToSuperview().offset(20)
-            make.trailing.equalToSuperview().offset(-20)
-            make.height.equalTo(self).multipliedBy(0.3)
-        }
-
-        midView.snp.makeConstraints { make in
-            make.top.equalTo(topView.snp.bottom).offset(16)
-            make.leading.equalToSuperview().offset(20)
-            make.trailing.equalToSuperview().offset(-20)
-            make.height.equalTo(152)
+    
+    // MARK: - 테이블 뷰 레이아웃
+    private func setupTableViewLayout() {
+        self.addSubview(tableView)
+        
+        tableView.snp.makeConstraints {
+            $0.top.equalTo(midView.snp.bottom).offset(20)
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.bottom.equalToSuperview()
         }
     }
+    
+    // MARK: - 상단 뷰 디테일
+    private func configureTopViewLayout() {
+        [locationLabel, locationDetailLabel, temperatureLabel, descriptionLabel].forEach {
+            topView.addSubview($0)
+        }
+        
+        locationLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(88)
+            $0.centerX.equalToSuperview()
+            $0.height.greaterThanOrEqualTo(32)
+        }
+        
+        locationDetailLabel.snp.makeConstraints {
+            $0.top.equalTo(locationLabel.snp.bottom).offset(8)
+            $0.centerX.equalToSuperview()
+            $0.height.greaterThanOrEqualTo(16)
+        }
+        
+        temperatureLabel.snp.makeConstraints {
+            $0.top.equalTo(locationDetailLabel.snp.bottom).offset(8)
+            $0.centerX.equalToSuperview()
+            $0.height.greaterThanOrEqualTo(48)
+        }
+        
+        descriptionLabel.snp.makeConstraints {
+            $0.top.equalTo(temperatureLabel.snp.bottom).offset(8)
+            $0.centerX.equalToSuperview()
+            $0.height.greaterThanOrEqualTo(16)
+        }
+    }
+    
+    // MARK: - 중단 뷰 디테일
+    private func configureMidViewLayout() {
+        [humidityLabel, humidityDetailLabel].forEach {
+            humidityContainer.addSubview($0)
+        }
+        
+        humidityLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(8)
+            $0.leading.equalToSuperview().offset(30)
+            $0.height.equalTo(16)
+        }
+        
+        humidityDetailLabel.snp.makeConstraints {
+            $0.top.equalTo(humidityLabel.snp.bottom).offset(8)
+            $0.leading.equalTo(humidityLabel.snp.leading)
+            $0.bottom.equalToSuperview().offset(-16)
+            $0.height.equalTo(48)
+        }
+        
+        [sunriseLabel, sunriseDetailLabel, sunsetLabel, sunsetDetailLabel].forEach {
+            sunriseSunsetContainer.addSubview($0)
+        }
+        
+        sunriseLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(24)
+            $0.leading.equalToSuperview().offset(24)
+            $0.height.equalTo(16)
+        }
+        
+        sunriseDetailLabel.snp.makeConstraints {
+            $0.top.equalTo(sunriseLabel.snp.bottom).offset(8)
+            $0.leading.equalTo(sunriseLabel.snp.leading)
+            $0.height.equalTo(16)
+        }
+        
+        sunsetLabel.snp.makeConstraints {
+            $0.top.equalTo(sunriseDetailLabel.snp.bottom).offset(16)
+            $0.leading.equalTo(sunriseLabel.snp.leading)
+            $0.height.equalTo(16)
+        }
+        
+        sunsetDetailLabel.snp.makeConstraints {
+            $0.top.equalTo(sunsetLabel.snp.bottom).offset(8)
+            $0.leading.equalTo(sunriseLabel.snp.leading)
+            $0.bottom.equalToSuperview().offset(-16)
+            $0.height.equalTo(16)
+        }
+    }
+    
+    // 헤더 뷰 관련
+    private func configureTableViewHeader() {
+        tableView.tableHeaderView = createTableViewHeader()
+    }
+    
+    // MARK: - 커스텀 헤더 뷰
+    private func createTableViewHeader() -> UIView {
+        let headerView = UIView()
+        headerView.backgroundColor = UIColor.systemBlue
+        headerView.layer.cornerRadius = 10
+        headerView.layer.masksToBounds = true
+        
+        let headerLabel = UILabel()
+        headerLabel.text = "5일간의 일기예보"
+        headerLabel.font = UIFont.boldSystemFont(ofSize: 12)
+        headerLabel.textColor = .systemGray5
+        headerLabel.textAlignment = .left
+        
+        headerView.addSubview(headerLabel)
+        
+        headerLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(16)
+            $0.trailing.equalToSuperview().offset(-16)
+            $0.centerY.equalToSuperview()
+        }
+        
+        headerView.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 30)
+        
+        return headerView
+    }
 }
-
-
