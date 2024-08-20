73427eb77d5a8ffc770452716aabdc986eb00a00
dx._.xk7 <polcet112@gmail.com>
2024-08-20 14:53:30 +0900

    [Feat] TextField -> 좌표로 변경

--- WeatherApp/ViewModel/SearchViewModel.swift
+++ WeatherApp/ViewModel/SearchViewModel.swift
@@ -14,20 +14,17 @@ class SearchViewModel {
     
     //view가 구독할 Subject
     let locationInfoSubject = PublishSubject<address>()
+    let text = BehaviorSubject<String>(value: "")
     let network = NetworkManager.shared
     
     //MARK: - 검색한 위치의 X, Y 좌표 구하기
-    func fetchLocation(){
+    func fetchLocation(query: String){
         let endpoint = Endpoint (
             baseURL: "https://dapi.kakao.com",
             headerParameters: ["Authorization": "KakaoAK 3c7a90563f65e8afc9ebfac9b753c698"],
             path: "/v2/local/search/address",
-            queryParameters: ["query" :"전북 삼성동 100"]
+            queryParameters: ["query" :"\(query)"]
         )
-//                
-//        curl -v -G GET "https://dapi.kakao.com/v2/local/search/address.json" \
-//          -H "Authorization: KakaoAK ${REST_API_KEY}" \
-//          --data-urlencode "query=전북 삼성동 100"
         
         network.fetch(endpoint: endpoint)
             .subscribe(onSuccess: { [weak self] (result: address) in
@@ -38,5 +35,11 @@ class SearchViewModel {
                 print("called ERROR Search ViewModel: \(error)")
             }).disposed(by: disposeBag)
     }
+    func updateText(_ newText: String) {
+            text.onNext(newText)
+            fetchLocation(query: newText)
+        }
+
+
 }
     
